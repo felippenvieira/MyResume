7b45c37a7d2383b0f28c61c0996b274c89ce35f1
Felippe <felippe_nvieira@hotmail.com>
2022-10-25 15:57:20 -0300

    contact constructor and methods

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -5,5 +5,33 @@
         public string Phone { get; private set; } = string.Empty;
         public string Email { get; private set; } = string.Empty;
         public List<string> SocialMedia { get; private set; }
+
+        public Contact(string phone, string email, List<string> socialMedia)
+        {
+            Phone = phone;
+            Email = email;
+            SocialMedia = socialMedia;
+        }
+
+        public Contact(string email, List<string> socialMedia)
+        {
+            Email = email;
+            SocialMedia = socialMedia;
+        }
+
+        public void UpdatePhone(string phone)
+        {
+            Phone = phone;
+        }
+
+        public void UpdateEmail(string email)
+        {
+            Email = email;
+        }
+
+        public void AddSocialMedia(string socialMedia)
+        {
+            SocialMedia.Add(socialMedia);
+        }
     }
 }
