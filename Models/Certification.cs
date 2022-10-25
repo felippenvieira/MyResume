05f0e4fb628b6c58b55e8cc48be16f9b5b8094c2
Felippe <felippe_nvieira@hotmail.com>
2022-10-25 16:13:59 -0300

    certification ctor and methods

--- Models/Certification.cs
+++ Models/Certification.cs
@@ -4,5 +4,21 @@
     {
         public string Name { get; private set; } = string.Empty;
         public string Company { get; private set; } = string.Empty;
+
+        public Certification(string name, string company)
+        {
+            Name = name;
+            Company = company;
+        }
+
+        public void ChangeCertificationName(string name)
+        {
+            Name = name;
+        }
+
+        public void ChangeCompanyName(string name)
+        {
+            Company = name;
+        }
     }
 }
