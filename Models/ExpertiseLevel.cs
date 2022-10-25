f861904af7ea6579e517509a005da9ea4dc369a8
Felippe <felippe_nvieira@hotmail.com>
2022-10-25 15:49:55 -0300

    modeling skill and expertise level enum

---
+++ Models/ExpertiseLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyResume
+{
+    public enum ExpertiseLevel
+    {
+        Basic = 0,
+        Intermediate = 1,
+        Advanced = 2,
+        Mastery = 3
+    }
+}
---
+++ Models/Skill.cs
@@ -0,0 +1,8 @@
+﻿namespace MyResume.Models
+{
+    public class Skill : BaseEntity
+    {
+        public string Name { get; private set; } = string.Empty;
+        public ExpertiseLevel ExpertiseLevel { get; private set; }
+    }
+}
