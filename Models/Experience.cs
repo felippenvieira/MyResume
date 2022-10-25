4356b0bb8da2fb8ab9c8038a6b25e26c41059e82
Felippe <felippe_nvieira@hotmail.com>
2022-10-25 15:44:21 -0300

    Modeling experience

---
+++ Models/Experience.cs
@@ -0,0 +1,12 @@
+﻿namespace MyResume.Models
+{
+    public class Experience : BaseEntity
+    {
+        public string CompanyName { get; private set; } = string.Empty;
+        public string Role { get; private set; } = string.Empty;
+        public DateTime Begin { get; private set; }
+        public DateTime End { get; private set; }
+        public string Location { get; private set; } = string.Empty;
+        public string Summary { get; private set; } = string.Empty;
+    }
+}
