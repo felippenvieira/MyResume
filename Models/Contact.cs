f1678a948c9f0b68684dfddfc6fb6c9b654851ec
Felippe <felippe_nvieira@hotmail.com>
2022-10-25 15:41:18 -0300

    Modeling contact

---
+++ Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace MyResume.Models
+{
+    public class Contact : BaseEntity
+    {
+        public string Phone { get; private set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
+        public List<string> SocialMedia { get; private set; }
+    }
+}
