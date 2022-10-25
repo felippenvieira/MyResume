e57c3515c0f3f66ae9d7df7ea0aec0d7250e724e
Felippe <felippe_nvieira@hotmail.com>
2022-10-25 16:12:31 -0300

    skill ctor and methods

--- Models/Skill.cs
+++ Models/Skill.cs
@@ -4,5 +4,21 @@
     {
         public string Name { get; private set; } = string.Empty;
         public ExpertiseLevel ExpertiseLevel { get; private set; }
+
+        public Skill(string name, ExpertiseLevel expertiseLevel)
+        {
+            Name = name;
+            ExpertiseLevel = expertiseLevel;
+        }
+
+        public void ChangeSkillName(string name)
+        {
+            Name = name;
+        }
+
+        public void ChangeExpertiseLevel(ExpertiseLevel expertiseLevel)
+        {
+            ExpertiseLevel = ExpertiseLevel;
+        }
     }
 }
