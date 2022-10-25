0cec9dd28395adc3dce597a1f5b9b00110642540
Felippe <felippe_nvieira@hotmail.com>
2022-10-25 15:38:17 -0300

    Modeling me

---
+++ Models/Me.cs
@@ -0,0 +1,11 @@
+﻿namespace MyResume.Models
+{
+    public class Me : BaseEntity
+    {
+        public string Name { get; private set; } = string.Empty;
+        public string Role { get; private set; } = string.Empty;
+        public string Summary { get; private set; } = string.Empty;
+        public Contact Contact { get; private set; }
+        public Experience Experience { get; private set; }
+    }
+}
